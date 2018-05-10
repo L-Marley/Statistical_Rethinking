e61798a9a6dc3fd51d9f5072c54d34379e034345
Liam Marley <marley_liam@hotmail.com>
2018-05-10 21:48:18 +0100

    Additional setup - loading of package for the book

---
+++ install_load_packages.R
@@ -0,0 +1,5 @@
+devtools::install_github("rmcelreath/rethinking")
+library(rethinking)
+
+# note, rstan is necessary and needs to be set up appropriately see: 
+# https://github.com/stan-dev/rstan/wiki/RStan-Getting-Started
